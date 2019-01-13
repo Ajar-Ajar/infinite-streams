bab27a3dd59914d5139c09f8210a33ecd273a328
Yariv Gilad aka Ajar Bahamonde <yariv.gilad@gmail.com>
2019-01-13 08:25:30 +0200

    added some consumer options

--- consumer.js
+++ consumer.js
@@ -1,19 +1,41 @@
 
+const marker = require('@ajar/marker')
 const pump = require('pump')
+const { pipeline } = require('stream');
 const ndjson = require('ndjson')
 const through = require('through2')
 const hyperquest = require('hyperquest')
-const marker = require('@ajar/marker')
 require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
-pump(
-  hyperquest(`http://localhost:${port}`),
-  ndjson.parse(),
-  through.obj(write),
-  process.stdout
-)
+
+  hyperquest(`http://localhost:${port}`)
+        .pipe(ndjson.parse())
+        .pipe(through.obj(write))
+        .pipe(process.stdout)
+
+
+// pump(
+//   hyperquest(`http://localhost:${port}`),
+//   ndjson.parse(),
+//   through.obj(write),
+//   process.stdout
+// )
+
+// pipeline(
+//   hyperquest(`http://localhost:${port}`),
+//   ndjson.parse(),
+//   through.obj(write),
+//   process.stdout,
+//   (err) => {
+//     if (err) {
+//       marker.error('Pipeline failed.', err);
+//     } else {
+//       marker.log('Pipeline succeeded.');
+//     }
+//   }
+// )
 
 function write (row, enc, next) {
   next(null, String(row.value * row.value) + '\n')
